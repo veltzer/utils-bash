3702afd885da0cfe7268943b1d385acfb77fc84e
Mark Veltzer <mark.veltzer@gmail.com>
2019-07-13 22:41:23 +0300

    move to 120

--- src/youtube_download_some.sh
+++ src/youtube_download_some.sh
@@ -9,6 +9,6 @@ youtube-dl\
 	https://www.youtube.com/playlist?list=PLZHKR1yYunFQ3ciN2kHYg5y7YjKZUjMey\
 	--continue\
 	--ignore-errors\
-	--playlist-end 60
+	--playlist-end 120
 #	--playlist-start 15
 utils_hibernate_enable.sh
