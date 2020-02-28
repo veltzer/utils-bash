8138349cc203c25b7f2ab59f84578c17e9611184
Mark Veltzer <mark.veltzer@gmail.com>
2020-02-28 20:01:54 +0200

    update

--- src/youtube_download_some.sh
+++ src/youtube_download_some.sh
@@ -1,14 +1,15 @@
 # make this script more complicated: make it download the data
 # one by one and stop when he sees he already downloaded something.
 # turn it input python maybe?
+# saw finished
+# https://www.youtube.com/playlist?list=PLZHKR1yYunFQ3ciN2kHYg5y7YjKZUjMey\
+# saw
+# https://www.youtube.com/playlist?list=PLZHKR1yYunFR7sXhq3hA89NLRLoFcEwmR\
 
 utils_hibernate_disable.sh
 # update youtube-dl
 youtube-dl -U
 youtube-dl\
-#	saw finished
-#	https://www.youtube.com/playlist?list=PLZHKR1yYunFQ3ciN2kHYg5y7YjKZUjMey\
-#	saw
 	https://www.youtube.com/playlist?list=PLZHKR1yYunFR7sXhq3hA89NLRLoFcEwmR\
 	--continue\
 	--ignore-errors\
