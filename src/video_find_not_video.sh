96fb7c7db7d02620b65b5688ad660d7405361b44
Mark Veltzer <mark.veltzer@gmail.com>
2020-04-09 18:46:14 +0300

    new script starts to work

--- src/video_find_not_video.sh
+++ src/video_find_not_video.sh
@@ -6,9 +6,23 @@ This script finds all files under the current folder which are not video files.
 
 COMMENT
 
-find\
-.\
--type -f\
--and -not -name "*.avi"\
--and -not -name "*.flv"\
--and -not -name "*.mp4"
+find . \
+-mindepth 2 \
+-type f \
+-and -not -name "*.avi" \
+-and -not -name "*.flv" \
+-and -not -name "*.wmv" \
+-and -not -name "*.mpg" \
+-and -not -name "*.asf" \
+-and -not -name "*.mpeg" \
+-and -not -name "*.rm" \
+-and -not -name "*.mov" \
+-and -not -name "*.mkv" \
+-and -not -name "*.3gp" \
+-and -not -name "*.ram" \
+-and -not -name "*.m4v" \
+-and -not -name "*.rmvb" \
+-and -not -name "*.qt" \
+-and -not -name "*.mp4" \
+-and -not -name "*.VOB" \
+-and -not -name "*.m3u"
