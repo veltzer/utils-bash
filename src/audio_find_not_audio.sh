2e60936c727cb406d35b48df4ae8e9ecc977bf16
Mark Veltzer <mark.veltzer@gmail.com>
2020-04-10 00:50:25 +0300

    more script

---
+++ src/audio_find_not_audio.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+<<'COMMENT'
+
+This script finds all files under the current folder which are not audio files.
+
+COMMENT
+
+find . \
+-mindepth 2 \
+-type f \
+-and -not -name "*.mp3" \
+-and -not -name "*.m3u"
