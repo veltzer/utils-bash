3ead364de096ab610f83942786daac3de0620bfc
Mark Veltzer <mark.veltzer@gmail.com>
2020-04-10 10:29:30 +0300

    another script

---
+++ src/youtube_download_saw0_latest.sh
@@ -0,0 +1,7 @@
+utils_hibernate_disable.sh
+youtube-dl\
+	https://www.youtube.com/playlist?list=PLZHKR1yYunFQ3ciN2kHYg5y7YjKZUjMey\
+	--continue\
+	--ignore-errors\
+	--playlist-end 20
+utils_hibernate_enable.sh
