ff70301ebc970ad3a9cd6328f9a2ae8a538cff7e
Mark Veltzer <mark.veltzer@gmail.com>
2020-04-13 09:25:21 +0300

    new tool for txt to pdf

---
+++ src/txt2pdf.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+<<'COMMENT'
+
+This script converts a text file to pdf using the combination of
+2 tools:
+- enscript: converts between text and postscript
+- ps2pdf: converts between postscript and pdf
+
+References:
+- https://unix.stackexchange.com/questions/17406/how-to-convert-txt-to-pdf
+
+COMMENT
+
+if [ $# -ne 2 ]
+then
+	echo "Usage: $0 [input.txt] [output.pdf]"
+	exit 1
+fi
+
+tmpfile=$(mktemp /tmp/txt2pdf.sh.XXXXXX)
+enscript "$1" -p "$tmpfile"
+ps2pdf "$tmpfile" "$2"
+rm "$tmpfile"
