6999849e3af037dd043056541269f7c0add2359c
Mark Veltzer <mark.veltzer@gmail.com>
2020-04-13 16:47:14 +0300

    added a few extensions

--- src/emovies_check.sh
+++ src/emovies_check.sh
@@ -2,7 +2,7 @@
 
 # this checks that the files are of the right type
 echo "Checking that all are of the right suffix"
-find by_name -mindepth 2 -type f -and -not -name "*.avi" -and -not -name "*.m4v" -and -not -name "*.mp4" -and -not -name "*.wmv" -and -not -name "*.mov" -and -not -name "*.flv"
+find by_name -mindepth 2 -type f -and -not -name "*.avi" -and -not -name "*.m4v" -and -not -name "*.mp4" -and -not -name "*.wmv" -and -not -name "*.mov" -and -not -name "*.flv" -and -not -name "*.mkv" -and -not -name "*.webm"
 # this checks for permissions other than 444
 echo "Permission problems"
 find by_name -mindepth 2 -type f -and -not -perm 444
