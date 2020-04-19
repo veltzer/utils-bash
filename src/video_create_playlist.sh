c06f717d5af8e2ca146644e332ee2d45d907f05d
Mark Veltzer <mark.veltzer@gmail.com>
2020-04-19 04:57:26 +0300

    new util

---
+++ src/video_create_playlist.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+<<'COMMENT'
+
+This script creates a playlist from the current folder
+
+COMMENT
+
+find . \
+-type f \
+-and \
+\( \
+-name "*.avi" \
+-or -name "*.flv" \
+-or -name "*.wmv" \
+-or -name "*.mpg" \
+-or -name "*.asf" \
+-or -name "*.mpeg" \
+-or -name "*.rm" \
+-or -name "*.mov" \
+-or -name "*.mkv" \
+-or -name "*.3gp" \
+-or -name "*.ram" \
+-or -name "*.m4v" \
+-or -name "*.rmvb" \
+-or -name "*.qt" \
+-or -name "*.mp4" \
+-or -name "*.VOB" \
+-or -name "*.m3u" \
+\) > playlist.m3u
