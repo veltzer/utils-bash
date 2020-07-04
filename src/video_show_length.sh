2a9619b823e743651f79e038cb7d335c9f0ec4ab
Mark Veltzer <mark.veltzer@gmail.com>
2020-07-04 10:58:41 +0300

    script now works

--- src/video_show_length.sh
+++ src/video_show_length.sh
@@ -8,6 +8,6 @@ COMMENT
 
 for vid in "$@"
 do
-	echo -n "$vid "
-	ffprobe -v quiet -of csv=p=0 -show_entries format=duration ${vid}
+	echo -n "${vid} "
+	ffprobe -v quiet -of csv=p=0 -show_entries format=duration "${vid}"
 done
