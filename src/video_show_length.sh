25f66a1169e7f965b384c6530a23be38dbd29d74
Mark Veltzer <mark.veltzer@gmail.com>
2020-07-04 10:54:02 +0300

    new script

---
+++ src/video_show_length.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+<<'COMMENT'
+
+This script receives video files on the command line and displays their length
+
+COMMENT
+
+for vid in "$@"
+do
+	echo -n "$vid "
+	ffprobe -v quiet -of csv=p=0 -show_entries format=duration ${vid}
+done
