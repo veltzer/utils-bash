65862add99aaf43e1b06e3253e964abde8215a36
Mark Veltzer <mark.veltzer@gmail.com>
2021-01-08 02:49:49 +0200

    fixed the script

--- src/system_sys_remove_cruft.sh
+++ src/system_sys_remove_cruft.sh
@@ -10,20 +10,14 @@ three things it does:
 
 COMMENT
 
-#if test $USER != 'root'
-#then
-#	echo "run me as root..."
-#	exit 1
-#fi
-
 # count number of rc packages...
 count=$(dpkg --list | grep "^rc" | tr -s " " | cut -d " " -f 2 | wc -l)
 # make sure that there are packages like that...
 if test "$count" -gt 0
 then
 	# this purges removed but not purge packages...
-	read -ra packages < <(dpkg --list | grep "^rc" | tr -s " " | cut -d " " -f 2)
-	sudo dpkg --purge  ${#packages[@]}
+	readarray -t packages < <(dpkg --list | grep "^rc" | tr -s " " | cut -d " " -f 2)
+	sudo dpkg --purge "${packages[@]}"
 else
 	echo "you do not have any rc package..."
 fi
