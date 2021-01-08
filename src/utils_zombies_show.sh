0076cfb1c92675e5bb972de86ce36e4a17adc220
Mark Veltzer <mark.veltzer@gmail.com>
2021-01-08 02:59:50 +0200

    better script

--- src/utils_zombies_show.sh
+++ src/utils_zombies_show.sh
@@ -5,9 +5,13 @@
 # Here are some other options for implementation:
 #     ps aux | awk '{ print $8 " " $2 }' | grep -w Z
 #     ps axo stat,pid,ppid,comm | grep -w defunct
-#     read -ra pids < <(pgrep -r S)
+#     read -ra pids < <(pgrep -r Z)
 #
 # TODO: rewrite this script in python and so be more accurate and flexible.
 #
 
-ps -o stat,pid,ppid,comm -p "$(pgrep -r S -d ' ')"
+readarray pids < <(pgrep --runstates Z)
+if test "${#pids[@]}" -gt 0
+then
+	ps -o stat,pid,ppid,comm -p "${pids[@]}"
+fi
