c9f1f9cc2bbe44b093ed6381cd1afe0a12a5d5a1
Mark Veltzer <mark.veltzer@gmail.com>
2021-01-23 22:53:52 +0200

    docker_update_all script added

---
+++ src/docker_update_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+: <<'COMMENT'
+
+This script updates all docker images
+
+References:
+- https://www.googlinux.com/update-all-docker-images/
+
+COMMENT
+
+for x in $(docker images --format "{{.Repository}}")
+do
+	docker pull "$x"
+done
+# docker images | grep -v REPOSITORY | awk '{print $1}' | xargs -L1 docker pull
