14f1be546acc4ed78a453a89b87e6067126bf4b8
Mark Veltzer <mark.veltzer@gmail.com>
2021-06-04 05:07:52 +0300

    new script

---
+++ src/video_create_stats.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+: <<'COMMENT'
+
+This script creates sorted stats for a video folder by scanning
+every folder 2 deep in and finding how many videos it has and
+then sorting everything.
+
+COMMENT
+
+find by_name -mindepth 2 -maxdepth 2 -and -type d > name_list.txt
+rm name_list_count.txt
+while IFS= read -r line
+do
+	count=$(find "$line" -type f | wc -l)
+	echo "$line $count" >> name_list_count.txt
+done < name_list.txt
+sort name_list_count.txt -r -n -k 2 > name_list_count_sorted.txt
