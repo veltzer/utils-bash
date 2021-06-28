3f17982c8b17f7c8d4dd216d01d7d81eb13dae16
Mark Veltzer <mark.veltzer@gmail.com>
2021-06-28 09:56:12 +0300

    more scripts

---
+++ src/apt_proxy_setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ -z "$http_proxy" -o -z "$https_proxy" ]
+then
+	echo "This script must be called when proxy variables are set"
+	exit 1
+fi
+sudo bash -c "cat << EOF > /etc/apt/apt.conf.d/21proxy
+Acquire {
+  HTTP::proxy "$http_proxy";
+  HTTPS::proxy "$https_proxy";
+}
+EOF"
