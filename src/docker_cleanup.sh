65e5490996e67484499447dab8a68eb79cfe06f4
Mark Veltzer <mark.veltzer@gmail.com>
2021-07-05 09:56:22 +0300

    new script docker_cleanup.sh

---
+++ src/docker_cleanup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script cleans up after docker
+# Use it after
+# $ sudo systemctl stop docker.service
+# because it leaves bridge interfaces which it does not clean up
+
+for x in $(ip -br link show type bridge | tr -s " " | cut -f 1 -d " ")
+do
+	sudo ip link del "$x"
+done
