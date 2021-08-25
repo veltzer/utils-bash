6510d060618b37c542966dd09ea13dfdb31d34de
Mark Veltzer <mark.veltzer@gmail.com>
2021-08-26 00:41:40 +0300

    renaming

--- src/utils_symlinks_find_bad.sh
+++ src/utils_symlinks_find_bad.sh
@@ -4,12 +4,19 @@
 
 This is a small script to find bad symbolic links and printout their
 names.
-TODO: rewrite this script in python to be more extendble and possible
+TODO: rewrite this script in python to be more extendable and possibly
 a lot faster.
 
 COMMENT
 
-find . -type l -and -not -exec test -e {} \; -print
+#find . -type l -and -not -exec test -e {} \; -print
 # it seems that firefox always creates a lock dangling symlink which
 # I have no great desire to see.
-#find . -type l -and -not -exec test -e {} \; -and -not -ipath "*/firefox/*" -print
+find .\
+	-type l\
+	-and -not -exec test -e {} \;\
+	-and -not -ipath "*/firefox/*"\
+	-and -not -ipath "*/google-chrome/*"\
+	-and -not -ipath "*/spyder.lock"\
+	-and -not -ipath "*/whatsdesk/*"\
+	-print
