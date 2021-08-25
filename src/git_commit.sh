ee81cf588a89eda2c297076b5fb7fab952fefad8
Mark Veltzer <mark.veltzer@gmail.com>
2021-08-25 11:15:37 +0300

    better version

--- src/git_commit.sh
+++ src/git_commit.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-git commit -am "commit message"
+if [[ $# -gt 1 ]]
+then
+	echo "You must pass at most one argument to this script..."
+	exit 1
+fi
+if [[ $# -eq 1 ]]
+then
+	msg="$1"
+else
+	msg="commit message"
+fi
+git commit -am $msg
 git push
