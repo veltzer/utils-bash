a7ba38a91f80e22fa31d192545b12360d92f566b
Mark Veltzer <mark.veltzer@gmail.com>
2022-04-22 03:26:11 +0300

    more stuff

--- src/github_rename_main_to_master.sh
+++ src/github_rename_main_to_master.sh
@@ -4,5 +4,6 @@
 # - https://stackoverflow.com/questions/30590083/how-do-i-rename-both-a-git-local-and-remote-branch-name
 
 git branch -m master
-git push origin :main master
-git push origin -u master
+git push origin master
+# git push origin --delete main
+# git push origin -u master
