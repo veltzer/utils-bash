b24359331de3ebb96e6ed31897d5db2897498627
Mark Veltzer <mark.veltzer@gmail.com>
2022-04-28 04:29:51 +0300

    moved to pygitpub

--- src/github_rename_main_to_master.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-# References:
-# - https://stackoverflow.com/questions/30590083/how-do-i-rename-both-a-git-local-and-remote-branch-name
-
-git branch -m master
-git push origin master
-# git push origin --delete main
-# git push origin -u master
