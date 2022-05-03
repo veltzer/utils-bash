97c0c170f99f665cf54b56e91791ac40926d53df
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-04 02:57:46 +0300

    better workflow

--- src/mg_check_same_files.sh
+++ src/mg_check_same_files.sh
@@ -15,7 +15,7 @@ pycmdtools mcmp --print "templates/setup.py.mako" py*/templates/setup.py.mako
 pycmdtools mcmp --print "templates/README.rst.mako" py*/templates/README.rst.mako
 pycmdtools mcmp --print "templates/README.md.mako" py*/templates/README.md.mako
 pycmdtools mcmp --print "templates/[pkg]/static.py.mako" py*/templates/*/static.py.mako
-pycmdtools mcmp --print "templates/.github/workflows/build.yml.mako" py*/templates/.github/workflows/build.yml.mako
+pycmdtools mcmp --print "templates/.github/workflows/build.yml.mako" */templates/.github/workflows/build.yml.mako
 # configs
 pycmdtools mcmp --print "config/apt.py" py*/config/apt.py
 pycmdtools mcmp --print "config/composites.py" py*/config/composites.py
@@ -28,4 +28,4 @@ pycmdtools mcmp --print "config/deps.py" py*/config/deps.py
 # .idea stuff
 pycmdtools mcmp --print ".idea/.gitignore" py*/.idea/.gitignore
 # results of templates
-pycmdtools mcmp --print ".github/workflows/build.yml" py*/.github/workflows/build.yml
+pycmdtools mcmp --print ".github/workflows/build.yml" */.github/workflows/build.yml
