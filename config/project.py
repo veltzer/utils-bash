fc904a962624f3b88a4e8ac5729aeefdc175e950
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:51:14 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,4 +1,1 @@
-project_name = "utils-bash"
-project_long_description = "Bash utilities"
-project_year_started = 2019
-project_description = """Bash utilities"""
+description_short = "Bash utilities"
