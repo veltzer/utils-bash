065e91504355739ebd3a4da837f58bc4f0d84921
Mark Veltzer <mark.veltzer@gmail.com>
2022-06-07 21:04:51 +0300

    more notes

--- src/video_to_mp4.sh
+++ src/video_to_mp4.sh
@@ -4,6 +4,12 @@
 
 This script converts everything you give it to mp3 using ffmpeg.
 
+TODO:
+the brand 'mp42' is hardcoded in this script. I readlly should be able
+to scrape that brand from an existing file and provide a functionality like:
+	$ video_to_mp4.sh --take-brand-from [example.mp4] [list of files to be converted]
+Maybe this will require to convert this script into python.
+
 COMMENT
 
 for input in "$@"
