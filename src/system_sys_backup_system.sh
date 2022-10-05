513d0552b38dbf072b13e89b234e2a3dc5a2f21d
Mark Veltzer <mark.veltzer@gmail.com>
2022-10-05 16:03:28 +0300

    better output

--- src/system_sys_backup_system.sh
+++ src/system_sys_backup_system.sh
@@ -24,12 +24,12 @@ TARGET_ALTERNATIVES="${FOLDER}/alternatives.${HOSTNAME}.txt"
 # we sudo since we are running as regular user and cannot enter into some folders...
 # we do not create the final output file but rather a file in /tmp because it will
 # have root ownership and we want a file with mark ownership.
-echo "creating ${TARGET_ETC}..."
+echo "creating [${TARGET_ETC}]..."
 sudo tar --create --bzip2 --absolute-names --file "${TMP_ETC}" /etc
 cp "${TMP_ETC}" "${TARGET_ETC}"
 sudo rm "${TMP_ETC}"
 # these two steps can be done by any user...
-echo "creating ${TARGET_DPKG_SELECTIONS}..."
+echo "creating [${TARGET_DPKG_SELECTIONS}]..."
 dpkg --get-selections > "${TARGET_DPKG_SELECTIONS}"
-echo "creating ${TARGET_ALTERNATIVES}..."
+echo "creating [${TARGET_ALTERNATIVES}]..."
 update-alternatives --get-selections > "${TARGET_DPKG_SELECTIONS}"
