742cd524354090d685b03fd6016aad18cab4ca4c
Mark Veltzer <mark.veltzer@gmail.com>
2022-11-01 11:15:08 +0200

    snap stuff

---
+++ src/snap_disable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo systemctl disable snapd.service
+sudo systemctl disable snapd.socket
+sudo systemctl disable snapd.seeded.service
+sudo systemctl mask snapd.service
+sudo systemctl mask snapd.socket
+sudo systemctl mask snapd.seeded.service
+sudo dpkg --purge snapd
---
+++ src/snap_remove.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# remove all snap packages and snap itself
+
+snaps_list=$(snap list | tail -n +2 | tr -s " " | cut -f 1 -d " ")
+for x in $snaps_list
+do
+	sudo snap remove "$x"
+done
