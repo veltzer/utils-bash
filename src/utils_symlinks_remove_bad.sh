7586d361bcae5d07c5232cd4e56676e17bc2155d
Mark Veltzer <mark.veltzer@gmail.com>
2022-12-10 17:42:05 +0200

    no commit message given

--- src/utils_symlinks_remove_bad.sh
+++ src/utils_symlinks_remove_bad.sh
@@ -4,4 +4,6 @@
 # names.
 # TODO: rewrite this script in python to be more extendble and possible
 # a lot faster.
-find . -type l -and -not -exec test -e {} \; -delete
+# the -print is to print every removed symlink, remove the flag if you
+# like this script to be more quiet.
+find . -type l -and -not -exec test -e {} \; -delete -print
