acb874aeedfdb242a85b70e81ca2c2f6ab150171
Mark Veltzer <mark.veltzer@gmail.com>
2023-04-02 13:10:45 +0300

    no commit message given

--- src/emovies_check.sh
+++ src/emovies_check.sh
@@ -10,7 +10,7 @@ fi
 echo "Checking that all are of the right suffix"
 find by_name -mindepth 2 -type f -and -not -name "*.avi" -and -not -name "*.m4v" -and -not -name "*.mp4" -and -not -name "*.wmv" -and -not -name "*.mov" -and -not -name "*.flv" -and -not -name "*.mkv" -and -not -name "*.webm" -and -not -name "link.txt"
 # this checks for permissions other than 444
-echo "Permission problems"
+echo "Permission problems (not 444)"
 find by_name -mindepth 2 -type f -and -not -perm 444
 echo "directory problems"
 find by_name -mindepth 4 -type f
