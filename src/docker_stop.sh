0bccf0273714357b78fd8ba3692fd2584b7c67bf
Mark Veltzer <mark.veltzer@gmail.com>
2023-04-07 22:20:38 +0300

    no commit message given

--- src/docker_stop.sh
+++ src/docker_stop.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -e
 
 docker network prune -f
-sudo systemctl stop docker.service containerd.service
+sudo systemctl --quiet stop docker.service containerd.service
 sudo ip link delete docker0
+if systemctl is-active --quiet docker.service
+then
+	status="up"
+else
+	status="down"
+fi
+echo "docker is [${status}]"
