7445f407f4c5df873604e635afc92f91e45c2b4e
Mark Veltzer <mark.veltzer@gmail.com>
2023-07-08 06:27:27 +0300

    no commit message given

--- src/system_sys_update_system.sh
+++ src/system_sys_update_system.sh
@@ -6,6 +6,16 @@ This script updates your system
 
 COMMENT
 
+FIX_MISSING="0"
+if [ "${FIX_MISSING}" == "1" ]
+then
+	ARGS_APTITUDE="-o APT::Get::Fix-Missing=true"
+	ARGS_APT_GET="--fix-missing"
+else
+	ARGS_APTITUDE=""
+	ARGS_APT_GET=""
+fi
+
 #if test $USER != 'root'
 #then
 #	echo "run me as root..."
@@ -18,7 +28,7 @@ sudo apt-get update
 # sudo apt-get -y dist-upgrades
 # sudo apt-get -y full-upgrade
 # this will install kept back packages as well
-sudo aptitude -y safe-upgrade
-sudo apt-get -y upgrade
+sudo aptitude -y safe-upgrade "${ARGS_APTITUDE}"
+sudo apt-get -y upgrade "${ARGS_APT_GET}"
 # sudo aptitude -y full-upgrade
 # sudo apt-get --with-new-pkgs upgrade
