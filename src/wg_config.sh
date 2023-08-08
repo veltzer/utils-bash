d1712d0acf39e9b1d8acff5743ac87eb9c6a1eb9
Mark Veltzer <mark.veltzer@gmail.com>
2023-08-08 14:48:14 +0300

    no commit message given

--- src/wg_config.sh
+++ src/wg_config.sh
@@ -12,7 +12,7 @@ Address = 172.32.0.4/24
 [Peer]
 PublicKey = \$PublicKey
 AllowedIPs = 172.31.0.0/16, 10.0.0.0/8, 169.254.169.253/32
-Endpoint = ec2-18-193-130-172.eu-central-1.compute.amazonaws.com:23894
+Endpoint = ec2-3-73-0-115.eu-central-1.compute.amazonaws.com:23894
 EOF
 
 sudo --preserve-env sh -c "envsubst < /tmp/wireguard.conf > /etc/wireguard/wireguard.conf"
