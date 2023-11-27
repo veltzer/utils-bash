fe3388f01d8b77c0ab2e5980cd8a9bcf9ea36827
Mark Veltzer <mark.veltzer@gmail.com>
2023-11-27 16:00:56 +0200

    no commit message given

---
+++ src/minikube_clean.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -e
+
+rm -rf ~/.minikube
+docker_clean_all.sh
---
+++ src/minikube_status.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+if systemctl is-active --quiet docker.service
+then
+	if [ "$(docker ps -q -f name=minikube)" ]
+	then
+		minikube status
+	else
+		echo "minikube is not running"
+	fi
+else
+	echo "docker is down, please bring it up"
+fi
