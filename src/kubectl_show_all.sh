4878a7b3251f436d9a2a7b8cfb8d542ff4c96a64
Mark Veltzer <mark.veltzer@gmail.com>
2023-12-17 23:48:25 +0200

    no commit message given

--- src/kubectl_show_all.sh
+++ src/kubectl_show_all.sh
@@ -2,4 +2,9 @@
 
 # Show all resources in my kubernetes cluister
 
-kubectl api-resources --verbs=list --namespaced -o name | xargs -n 1 kubectl get --show-kind --ignore-not-found -n ALL
+kubectl get pods
+kubectl get services
+kubectl get deployments
+kubectl get configmaps
+kubectl get secrets
+# kubectl api-resources --verbs=list --namespaced -o name | xargs -n 1 kubectl get --show-kind --ignore-not-found -n default
