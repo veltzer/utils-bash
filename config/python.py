db7a068707a7895e44b8feb6a86c4c608aee58b5
Mark Veltzer <mark.veltzer@gmail.com>
2023-12-30 04:01:49 +0200

    no commit message given

--- config/python.py
+++ config/python.py
@@ -1,4 +1,12 @@
-make_requires = [
+from typing import List
+
+
+config_requires: List[str] = []
+dev_requires: List[str] = []
+install_requires: List[str] = []
+make_requires: List[str] = [
     "pymakehelper",
     "pydmt",
 ]
+test_requires: List[str] = []
+requires = config_requires + install_requires + make_requires + test_requires
