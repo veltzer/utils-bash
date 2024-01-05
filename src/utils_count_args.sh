2810e3ab7ed2fb5c5bb2e3e6e8bbe6426db922f8
Mark Veltzer <mark.veltzer@gmail.com>
2024-01-05 16:45:04 +0200

    no commit message given

---
+++ src/utils_count_args.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+: <<'COMMENT'
+
+This is a simple bash script which just prints the number of arguments it gets.
+It is useful when you need to using globbing and count how many files you globbed.
+
+COMMENT
+
+echo "Number of arguments: $#"
