47bd2cb208cce4cf1bc5acf803788868bb904701
Mark Veltzer <mark.veltzer@gmail.com>
2024-01-12 15:07:46 +0200

    more python.py changes

--- config/python.py
+++ config/python.py
@@ -4,9 +4,9 @@ from typing import List
 config_requires: List[str] = []
 dev_requires: List[str] = []
 install_requires: List[str] = []
-make_requires: List[str] = [
+build_requires: List[str] = [
     "pymakehelper",
     "pydmt",
 ]
 test_requires: List[str] = []
-requires = config_requires + install_requires + make_requires + test_requires
+requires = config_requires + install_requires + build_requires + test_requires
