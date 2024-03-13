7393819873c211df2cf7f173010f51c106cacdd0
Mark Veltzer <mark.veltzer@gmail.com>
2024-03-13 13:01:15 +0200

    no commit message given

--- src/minikube_start.sh
+++ src/minikube_start.sh
@@ -3,5 +3,5 @@
 # We are passing "driver=docker" here but minikube will detect docker usually so it's not a must
 # the --keep-context is to keep minikube from overwriting ~/.kube/config
 # there is no --kubeconfig flag to "minikube start"
-export KUBECONFIG="${HOME}/.minikube.config"
+# export KUBECONFIG="${HOME}/.minikube.config"
 minikube start --driver=docker
