776b75a8d53ab2c8be9912a9ac27fb5ff7966602
Mark Veltzer <mark.veltzer@gmail.com>
2024-08-31 05:02:31 +0300

    no commit message given

--- src/code_install_extensions.sh
+++ src/code_install_extensions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 # Bash script to install VSCode extensions
 
-while read extension; do
-    code --install-extension "$extension"
+while read -r extension; do
+    code --install-extension "${extension}"
 done < "${HOME}/.vscode-extensions.txt"
