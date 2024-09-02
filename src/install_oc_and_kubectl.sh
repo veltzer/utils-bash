d325359edfcf3f157889280d1bcdc723ba16b1bb
Mark Veltzer <mark.veltzer@gmail.com>
2024-09-02 08:13:13 +0300

    no commit message given

--- src/install_oc_and_kubectl.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-cd ~/install/bin
-wget "https://github.com/openshift/okd/releases/download/4.7.0-0.okd-2021-03-28-152009/openshift-client-linux-4.7.0-0.okd-2021-03-28-152009.tar.gz"
-tar xvf "openshift-client-linux-4.7.0-0.okd-2021-03-28-152009.tar.gz"
-rm README.md "openshift-client-linux-4.7.0-0.okd-2021-03-28-152009.tar.gz"
