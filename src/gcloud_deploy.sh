b2aa77e863e69569496384e99f0a973718a0cf7d
Mark Veltzer <mark.veltzer@gmail.com>
2024-10-07 01:35:33 +0300

    no commit message given

--- src/gcloud_deploy.sh
+++ src/gcloud_deploy.sh
@@ -3,4 +3,4 @@
 gcloud app deploy --promote --stop-previous-version --quiet
 # remove the old version that does not get any more traffic
 version=$(gcloud app versions list --format=json | jq -r ".[] | select(.traffic_split == 0.0) | .id")
-gcloud app versions delete "${version}"
+gcloud app versions delete "${version}" --quiet
