af2aa2775276c9130a92e673dca67c840d453765
Mark Veltzer <mark.veltzer@gmail.com>
2024-10-06 23:41:01 +0300

    better script

--- src/gcloud_deployed_file_list.sh
+++ src/gcloud_deployed_file_list.sh
@@ -1,16 +1,15 @@
 #!/bin/bash -e
 
-if [ $# -ne 1 ]
-then
-	echo "Error: This script requires a version"
-	echo "You can get it from: gcloud app versions list"
-	echo "Usage: $0 [version]"
-	exit 1
-fi
+# if [ $# -ne 1 ]; then
+# 	echo "Error: This script requires a version"
+# 	echo "You can get it from: gcloud app versions list"
+# 	echo "Usage: $0 [version]"
+# 	exit 1
+# fi
+# version="$1"
 
 # get the latest version deployed
-# gcloud app version list --format=json | jq [...]
+version=$(gcloud app versions list --format=json | jq -r ".[0].id")
 
 service="default"
-version="$1"
 gcloud app versions describe "${version}" --service "${service}" --format=json | jq -r ".deployment.files | keys[]"
