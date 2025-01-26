79d491ec7c2addd3a541c57edcda14b94afef46c
Mark Veltzer <mark.veltzer@gmail.com>
2025-01-26 10:29:59 +0200

    no commit message given

--- src/docker_stop.sh
+++ src/docker_stop.sh
@@ -1,7 +1,29 @@
 #!/bin/bash -e
 
-sudo systemctl --quiet stop docker.service containerd.service
-sudo ip link delete docker0
+if systemctl is-active --quiet docker.service
+then
+	echo "docker.service is up, stopping it"
+	sudo systemctl --quiet stop docker.service containerd.service
+else
+	echo "docker.service is down. Good"
+fi
+
+if systemctl is-active --quiet containerd.service
+then
+	echo "containerd.service is up, stopping it"
+	sudo systemctl --quiet stop docker.service containerd.service
+else
+	echo "containerd.service is down. Good"
+fi
+
+if ip link show docker0 &> /dev/null
+then
+	echo "docker0 device is up, deleting it"
+	sudo ip link delete docker0
+else
+	echo "no docker0 device. Good"
+fi
+
 if systemctl is-active --quiet docker.service
 then
 	status="up"
