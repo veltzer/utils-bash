8a859cc449e80cfe7a5ab1e1b378314e8172ba11
Mark Veltzer <mark.veltzer@gmail.com>
2025-02-26 00:28:32 +0200

    no commit message given

--- src/minikube_status.sh
+++ src/minikube_status.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -e
 
-if systemctl is-active --quiet docker.service
-then
-	if [ "$(docker ps -q -f name=minikube)" ]
-	then
-		minikube status
-	else
-		echo "minikube is not running"
-	fi
-else
-	echo "docker is down, please bring it up"
-fi
+minikube status
+# if systemctl is-active --quiet docker.service
+# then
+# 	if [ "$(docker ps -q -f name=minikube)" ]
+# 	then
+# 		minikube status
+# 	else
+# 		echo "minikube is not running"
+# 	fi
+# else
+# 	echo "docker is down, please bring it up"
+# fi
