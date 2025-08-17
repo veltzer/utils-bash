72a3ae76cb5cb66c07cf801cf7c626277694a7c5
Mark Veltzer <mark.veltzer@gmail.com>
2025-08-17 12:16:50 +0300

    more stuff

--- src/screen_flip.sh
+++ src/screen_flip.sh
@@ -9,9 +9,12 @@ COMMENT
 if [ $# -ne 1 ]
 then
 	echo "No screen id specified"
+	echo "Screens are:"
+	xrandr --listmonitors
 	exit 1
 fi
 
 SCREEN_ID=$1
-xrandr --output "${SCREEN_ID}" --rotate inverted
+# xrandr --output "${SCREEN_ID}" --rotate left
+xrandr --output "${SCREEN_ID}" --rotate right
 # xrandr --output "${SCREEN_ID}" --rotate normal
