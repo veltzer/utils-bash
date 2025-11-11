3a6984b64bc8317b9eacb4ad5c063c24347b184b
Mark Veltzer <mark.veltzer@gmail.com>
2025-11-11 14:19:44 +0200

    no commit message given

--- src/code_install_extensions.sh
+++ src/code_install_extensions.sh
@@ -1,6 +1,13 @@
 #!/bin/bash -eu
 # Bash script to install VSCode extensions
 
-while read -r extension; do
-    code --force --install-extension "${extension}"
-done < "${HOME}/.vscode-extensions.txt"
+echo "removing all extensions"
+rm -rf ~/.vscode/extensions
+if [ -f ".vscode-extensions.txt" ]
+then
+	while read -r extension; do
+	    code --force --install-extension "${extension}"
+	done < ".vscode-extensions.txt"
+else
+	echo "you do not have a .vscode-extensions.txt file, stopping"
+fi
