3e48717f03e3812a0af4d02ae9897096cdf13eb1
Mark Veltzer <mark.veltzer@gmail.com>
2026-05-09 18:16:38 +0300

    no commit message given

--- src/gh_last_workflow_state.sh
+++ src/gh_last_workflow_state.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -eu
 
-PAGER= gh run list --repo veltzer/demos-os-linux --limit 1 --json conclusion --jq '.[0].conclusion'
+PAGER=gh run list --repo veltzer/demos-os-linux --limit 1 --json conclusion --jq '.[0].conclusion'
